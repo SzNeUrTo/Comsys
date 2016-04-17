6944d1f70dca0e1c1e0c1dcac74e8c4bad04df23
Supanut <supanut.overload@gmail.com>
2016-04-17 16:43:30 +0700

    Refactor mergesort.cpp
    Refactor

--- mergesort.cpp
+++ mergesort.cpp
@@ -5,51 +5,48 @@
 #define MAX 255
 #define MIN 0
 
-void Merge(int *A,int *L,int leftCount,int *R,int rightCount) {
-	int i,j,k;
-
+void merge(int *A, int *L, int leftCount, int *R, int rightCount) {
+	int i, j, k;
 	i = 0; j = 0; k =0;
 
-	while(i<leftCount && j< rightCount) {
-		if(L[i]  < R[j]) A[k++] = L[i++];
+	while(i < leftCount && j < rightCount) {
+		if(L[i] < R[j]) A[k++] = L[i++];
 		else A[k++] = R[j++];
 	}
 	while(i < leftCount) A[k++] = L[i++];
 	while(j < rightCount) A[k++] = R[j++];
 }
 
-void MergeSort(int *A,int n) {
-	int mid,i, *L, *R;
+void mergeSort(int *A, int n) {
+	int mid, *L, *R;
 	if(n < 2) return;
 
-	mid = n/2;
+	mid = n / 2;
 
-	L = (int*)malloc(mid*sizeof(int));
-	R = (int*)malloc((n- mid)*sizeof(int));
+	L = (int*) malloc(mid * sizeof(int));
+	R = (int*) malloc((n - mid) * sizeof(int));
 
-	for(i = 0;i<mid;i++) L[i] = A[i];
-	for(i = mid;i<n;i++) R[i-mid] = A[i];
+	for(int i = 0; i < mid; i++) L[i] = A[i];
+	for(int i = mid; i < n; i++) R[i - mid] = A[i];
 
-	MergeSort(L,mid);
-	MergeSort(R,n-mid);
-	Merge(A,L,mid,R,n-mid);
-        free(L);
-        free(R);
+	mergeSort(L, mid);
+	mergeSort(R, n - mid);
+	merge(A, L, mid, R, n - mid);
+	free(L);
+	free(R);
 }
 
 int main() {
-	int n = N;
-	int A[n];
-	for(int i = 0; i < n; i++) {
-		A[i] = rand() % (MAX+1) + MIN;
+	int A[N];
+	for(int i = 0; i < N; i++) {
+		A[i] = rand() % (MAX + 1) + MIN;
 	}
-	int i,numberOfElements;
 
-	numberOfElements = n;
+	mergeSort(A, N);
 
-	MergeSort(A,numberOfElements);
+	for(int i = 0;i < N; i++) {
+		printf("%d\n", A[i]);
+	}
 
-	for(i = 0;i < numberOfElements;i++)
-		printf("%d\n",A[i]);
 	return 0;
 }
