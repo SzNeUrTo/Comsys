1d88a9da49d255de31f523e06dc45f37ff3a5845
ak1103dev <ak.apichan1103@gmail.com>
2016-05-12 23:09:48 +0700

    client can send multiple message

--- prob3/client_socket2.c
+++ prob3/client_socket2.c
@@ -20,42 +20,38 @@ int main(int argc, char *argv[]) {
 
    portno = atoi(argv[1]);
 
-   /* Create a socket point */
-   sockfd = socket(AF_INET, SOCK_STREAM, 0);
-
-   if (sockfd < 0) {
-      perror("ERROR opening socket");
-      exit(1);
-   }
-
-   server = gethostbyname("localhost");
-
-   if (server == NULL) {
-      fprintf(stderr,"ERROR, no such host\n");
-      exit(0);
-   }
-
-   bzero((char *) &serv_addr, sizeof(serv_addr));
-   serv_addr.sin_family = AF_INET;
-   bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
-   serv_addr.sin_port = htons(portno);
-
-   /* Now connect to the server */
-   if (connect(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
-      perror("ERROR connecting");
-      exit(1);
-   }
-
    while(1) {
+	   /* Create a socket point */
+	   sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+	   if (sockfd < 0) {
+		  perror("ERROR opening socket");
+		  exit(1);
+	   }
+
+	   server = gethostbyname("localhost");
+
+	   if (server == NULL) {
+		  fprintf(stderr,"ERROR, no such host\n");
+		  exit(0);
+	   }
+
+	   bzero((char *) &serv_addr, sizeof(serv_addr));
+	   serv_addr.sin_family = AF_INET;
+	   bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
+	   serv_addr.sin_port = htons(portno);
+
+	   /* Now connect to the server */
+	   if (connect(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
+		  perror("ERROR connecting");
+		  exit(1);
+	   }
+
    /* Now ask for a message from the user, this message
       * will be read by server
    */
-   //while(strcmp(buffer, "end")) {
-	   //printf("Please enter the message: ");
 	   bzero(buffer,256);
 	   fgets(buffer,255,stdin);
-	   //strcpy(buffer, argv[2]);
-	   //printf("send: %s\n", buffer);
 
 	   /* Send message to the server */
 	   n = write(sockfd, buffer, strlen(buffer));
@@ -74,8 +70,8 @@ int main(int argc, char *argv[]) {
 		  exit(1);
 	   }
 
-	   printf("%s\n",buffer);
-	   close(sockfd);
+	   //printf("%s\n",buffer);
    }
+   close(sockfd);
    return 0;
 }
