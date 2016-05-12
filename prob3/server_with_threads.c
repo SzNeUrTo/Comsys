b8111466f66aa58787bc1388de010d0a24b3add8
ak1103dev <ak.apichan1103@gmail.com>
2016-05-12 21:39:39 +0700

    server can open 3 port

--- prob3/server_with_threads.c
+++ prob3/server_with_threads.c
@@ -1,66 +1,69 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
-
 #include <netdb.h>
 #include <netinet/in.h>
-
 #include <string.h>
+#include <unistd.h>
+
+#define NUM 3
 
 void doprocessing (int sock);
 
-void *operation() {
+void *operation(void *port) {
    int sockfd, newsockfd, portno, clilen;
    char buffer[256];
    struct sockaddr_in serv_addr, cli_addr;
    int n, pid;
-   
+
    /* First call to socket() function */
    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-   
+
    if (sockfd < 0) {
       perror("ERROR opening socket");
       exit(1);
    }
-   
+
    /* Initialize socket structure */
    bzero((char *) &serv_addr, sizeof(serv_addr));
-   portno = 5001;
-   
+   //portno = 5001;
+   portno = *((int *) port);
+   //printf("portno = %d\n", *((int *) port));
+
    serv_addr.sin_family = AF_INET;
    serv_addr.sin_addr.s_addr = INADDR_ANY;
    serv_addr.sin_port = htons(portno);
-   
+
    /* Now bind the host address using bind() call.*/
    if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
       perror("ERROR on binding");
       exit(1);
    }
-   
+
    /* Now start listening for the clients, here
       * process will go in sleep mode and will wait
       * for the incoming connection
    */
-   
+
    listen(sockfd,5);
    clilen = sizeof(cli_addr);
-   
+
    while (1) {
       newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
-		
+
       if (newsockfd < 0) {
          perror("ERROR on accept");
          exit(1);
       }
-      
+
       /* Create child process */
       pid = fork();
-		
+
       if (pid < 0) {
          perror("ERROR on fork");
          exit(1);
       }
-      
+
       if (pid == 0) {
          /* This is the client process */
          close(sockfd);
@@ -70,14 +73,21 @@ void *operation() {
       else {
          close(newsockfd);
       }
-		
+
    } /* end of while */
 }
 
 int main(int argc, char *argv[]) {
-	pthread_t thread;
-	pthread_create(&thread, NULL, operation, NULL);
-	pthread_join(thread, NULL);
+	pthread_t thread[NUM];
+	int port[NUM];
+	int i;
+	for(i = 0; i < NUM; i++) {
+		port[i] = atoi(argv[i+1]);
+		pthread_create(&thread[i], NULL, operation, (void*) &port[i]);
+	}
+	for(i = 0; i < NUM; i++) {
+		pthread_join(thread[i], NULL);
+	}
 	return 0;
 }
 
@@ -86,18 +96,18 @@ void doprocessing (int sock) {
    char buffer[256];
    bzero(buffer,256);
    n = read(sock,buffer,255);
-   
+
    if (n < 0) {
       perror("ERROR reading from socket");
       exit(1);
    }
-   
+
    printf("Here is the message: %s\n",buffer);
    n = write(sock,"I got your message",18);
-   
+
    if (n < 0) {
       perror("ERROR writing to socket");
       exit(1);
    }
-   
+
 }
